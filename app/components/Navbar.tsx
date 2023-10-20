9756eee2bde19ebcec9fa0fea4a19616a3133739
ChrisDTon <chranieon@gmail.com>
2023-10-20 13:49:20 +0200

    Something wrong with site generation

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <nav className='bg-slate-600 flex p-4 justify-between flex-col md:flex-row sticky top-0 drop-shadow-xl'>
         <h1 className='text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0'>
-            <Link href={'/'}>Shoep!</Link>
+            <Link href={'/'}>WikiRedirect!</Link>
         </h1>
         <Search />
     </nav>
--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -23,7 +23,7 @@ export default function Search() {
             placeholder='Search'
         />
         <button className='p-2 text-xl rounded-xl bg-slate-300 ml-2 font-bold'>
-            👟
+            🔎
         </button>
     </form>
   )
